51d7d8443b4549f9b22ac720680ed726d4459e58
Olek-sii <oleksii.al@gmail.com>
2017-11-04 00:05:23 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] }
+     ]
+   }
+};
